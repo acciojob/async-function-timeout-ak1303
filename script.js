ae72c88d29df4401e35fde5e689cf987670c8036
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-03-21 14:27:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If required.
 let submit = document.getElementById('btn');
 let msg="",delay=0;
+let textInput = document.getElementById('text');
 let output = document.createElement('div');
 let form = document.getElementsByTagName('form')[0];
 
-document.addEventListener('change',(e)=>{
+textInput.addEventListener('change',(e)=>{
 	if(e.target.id=='text')
 		msg=e.target.value;
 	if(e.target.id=='delay')
