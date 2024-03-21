c2fb5223bbe0611b596535c7be6641035e16053b
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-03-21 15:56:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-//your JS code here. If required.
 let submit = document.getElementById('btn');
-let msg="",delay=0;
 let textInput = document.getElementById('text');
-let output = document.createElement('div');
-let form = document.getElementsByTagName('form')[0];
+let delayInput = document.getElementById('delay');
+let output = document.getElementById('output');
 
-textInput.addEventListener('change',(e)=>{
-	if(e.target.id=='text')
-		msg=e.target.value;
-	if(e.target.id=='delay')
-		delay=parseInt(e.target.value);
-})
-submit.addEventListener('click',(e)=>{
-	// output.remove();
-	setTimeout(()=>{
-		output.innerHTML=msg;
-		form.append(output);
-	},delay);
-})
+let msg = "", delay = 0;
 
+textInput.addEventListener('change', (e) => {
+    msg = e.target.value;
+});
 
+delayInput.addEventListener('change', (e) => {
+    delay = parseInt(e.target.value);
+});
 
+submit.addEventListener('click', async (e) => {
+    await new Promise(resolve => setTimeout(resolve, delay));
+    output.innerHTML = msg;
+});
